55c2aa55d1a8eeae3377a568172c4e5c101a5679
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-04 23:33:10 +0300

    roles added

--- app.js
+++ app.js
@@ -2,20 +2,42 @@ const fastify = require('fastify')({
   logger: true
 })
 require('dotenv').config()
-/* const AutoLoad = require('fastify-autoload') */
+const AutoLoad = require('fastify-autoload')
 const formBody = require('fastify-formbody')
 const cors = require('fastify-cors')
 const path = require('path')
 const mongoose = require('mongoose')
-
+const jwt = require('jsonwebtoken')
 fastify.register(formBody)
-/* fastify.register(AutoLoad, {
+fastify.register(AutoLoad, {
   dir: path.join(__dirname, 'routes')
-}) */
+})
 fastify.register(cors, {
   origin: true
 })
-
+;async (req, res, next) => {
+  if (req.headers['x-access-token']) {
+    try {
+      const accessToken = req.headers['x-access-token']
+      const { userId, exp } = await jwt.verify(
+        accessToken,
+        process.env.JWT_SECRET
+      )
+      // If token has expired
+      if (exp < Date.now().valueOf() / 1000) {
+        return res.status(401).json({
+          error: 'JWT token has expired, please login to obtain a new one'
+        })
+      }
+      res.locals.loggedInUser = await User.findById(userId)
+      next()
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    next()
+  }
+}
 // Connect to DB
 mongoose
   .connect(process.env.MONGO_URI, { useUnifiedTopology: true })
