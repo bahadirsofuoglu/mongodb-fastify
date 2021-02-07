dace58191a4aaeec836eed65aedb42a611f81754
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-07 18:59:09 +0300

    transactionController completed

---
+++ controllers/transactionController.js
@@ -0,0 +1,40 @@
+const { User } = require('./models/user')
+const { Product } = require('./models/product')
+const { Transaction } = require('./models/transaction')
+const { ObjectID } = require('mongodb')
+exports.create = async (req, res) => {
+  const { product, paymentData, user_id } = req.body
+  const transactionData = {
+    email: paymentData.email,
+    data: paymentData,
+    product: product
+  }
+  // Update user purchase history
+  User.findByIdAndUpdate(
+    user_id,
+    {
+      $push: {
+        history: [
+          {
+            paymentId: paymentData.paymentID,
+            dateOfPurchase: Date.now(),
+            item: product
+          }
+        ]
+      }
+    },
+    { new: true },
+    err => {
+      if (err) return res.send({ success: false, err })
+    }
+  )
+  const payment = new Transaction(transactionData)
+  payment.save().then(
+    result => {
+      res.send(result)
+    },
+    error => {
+      res.status(400).send(error) // 400 for bad request
+    }
+  )
+}
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,20 @@
 const User = require('../models/user')
 exports.create = async (req, res) => {
-  /* Example Request Body
-   *  {
-   *      name: "mongoose",
-   *      email: "mongoose@mongodb.com",
-   *  }
-   */
-
   const user = new User(req.body)
   const result = await user.save()
   res.send(result)
+}
+exports.user = async (req, res) => {
+  const email = req.query.email
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        res.status(404).send()
+      } else {
+        res.send(user)
+      }
+    })
+    .catch(error => {
+      res.status(500).send() // server error
+    })
 }
