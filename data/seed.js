3e0feac7fb996f01bd9bac4072b9d26a1bb00866
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-08 11:10:51 +0300

    seed changes(product to category)

--- data/seed.js
+++ data/seed.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 // Connect to MongoDB via Mongoose
 seeder.connect(process.env.MONGO_URI, function () {
   // Load Mongoose models
-  seeder.loadModels(['models/product'])
+  seeder.loadModels(['models/category'])
 
   // Clear specified collections
-  seeder.clearModels(['product'], function () {
+  seeder.clearModels(['category'], function () {
     // Callback to populate DB once collections have been cleared
     seeder.populateModels(data, function () {
       seeder.disconnect()
@@ -17,13 +17,16 @@ seeder.connect(process.env.MONGO_URI, function () {
 // Data array containing seed data - documents organized by Model
 var data = [
   {
-    model: 'product',
+    model: 'category',
     documents: [
       {
-        name: 'bahadir',
-        category: 'fashion',
-        description: 'adsfasd',
-        price: '300'
+        name: 'Test1'
+      },
+      {
+        name: 'Test2'
+      },
+      {
+        name: 'Test3'
       }
     ]
   }
